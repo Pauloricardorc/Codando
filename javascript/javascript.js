08f731235b9a8c81d83fd4c270ae097b7977e4cb
Pauloricardorc <paulopvh30@gmail.com>
2020-04-17 15:45:17 -0300

    novo- animação, pasta de erro, imagems, css's, js

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,25 @@
-const texto = document.querySelector('.imagem-ico')
+const OpacityBody = document.querySelector(".body-contum");
+const OpacityBody1 = document.querySelector(".body-contdois");
+const OpacityBody2 = document.querySelector(".body-conttres");
 
-function as(){
-}
+function Mostrar1(){
+    console.log(OpacityBody.classList);
+    OpacityBody.classList.add('animated');
+    OpacityBody.classList.add('flipInX');
+    OpacityBody.classList.add('delay-0.5s');
+    OpacityBody.classList.remove('invisible');
+};
+function Mostrar2(){
+    console.log(OpacityBody.classList);
+    OpacityBody1.classList.add('animated');
+    OpacityBody1.classList.add('flipInX');
+    OpacityBody1.classList.add('delay-0.5s');
+    OpacityBody1.classList.remove('invisible');
+};
+function Mostrar3(){
+    console.log(OpacityBody.classList);
+    OpacityBody2.classList.add('animated');
+    OpacityBody2.classList.add('flipInX');
+    OpacityBody2.classList.add('delay-0.5s');
+    OpacityBody2.classList.remove('invisible');
+};
